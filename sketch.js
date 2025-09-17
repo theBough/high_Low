591b6c7d1594ad4a90d6e59a626fded62a95041b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-09-17 11:28:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,11 +2,15 @@ let myFont;
 
 function setup() {
   createCanvas(300, 300);
-  
+  myFont = loadFont("Oswald-VariableFont_wght-1.ttf")
 }
 
 function draw() {
   background('#003049');
-  fill('#FDF0D5')
-  text("my first game",200,50);  
+  fill('#FDF0D5');
+  textSize(30)
+  textFont(myFont)
+  text("High Low",100,50); 
+  textSize(18)
+  text("Guess a Number",100,100); 
 }
