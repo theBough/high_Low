0424c810ee02652b5afeef68ca7914f1031833f7
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-09-18 11:34:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
-let myFont, myInput;
+let myFont,
+    myInput,
+    randomNumber;
+
 
 function setup() {
   createCanvas(300, 300);
-  myFont = loadFont("Oswald-VariableFont_wght-1.ttf")
+  myFont = loadFont("FascinateInline-Regular.ttf")
   myInput = createInput();
   myInput.position(100,150)
+   randomNumber = Math.floor(Math.random()*100)
+  console.log(randomNumber)
+ 
 }
 
 function draw() {
@@ -15,5 +21,6 @@ function draw() {
   text("High Low",100,50); 
   textSize(18)
   text("Guess a Number",100,100); 
-  text(myInput.value(),100,200)
+  text(myInput.value(),100,200);
+ 
 }
