2c23c0f902c6d7affdb5701728eab99f40879c18
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-09-19 11:16:08 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,21 +3,14 @@ let myFont,
     randomNumber,
     myButton;
 
-
 function setup() {
   createCanvas(300, 300);
   myFont = loadFont("FascinateInline-Regular.ttf")
-  myInput = createInput();
-  myInput.position(100,150)
+ inputStuff();
    randomNumber = Math.floor(Math.random()*100)
   console.log(randomNumber)
-  myButton = createButton("Make a Guess");
-  myButton.style('background-color','#FDF0D5')
- 
-  myButton.position(100,180);
- 
-}
-
+  buttonStuff()
+}//end setup
 function draw() {
   background('#003049');
   fill('#FDF0D5');
@@ -28,4 +21,16 @@ function draw() {
   text("Guess a Number",100,100); 
   text(myInput.value(),100,200);
  
-}
+}//end draw
+//***********************************************
+function buttonStuff(){
+  myButton = createButton("Make a Guess");
+  myButton.style('background-color','#FDF0D5')
+ 
+  myButton.position(100,180);
+ 
+}//end buttonStuff
+function inputStuff(){
+   myInput = createInput();
+  myInput.position(100,150);
+}//end inputStuff
